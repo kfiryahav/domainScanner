c449df0ebcf2d9e9138062ea5cdc3d363d006f94
sapir yahav <kfiryahav12@gmail.com>
2023-08-21 19:40:09 +0300

    add rate limit function

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ require('dotenv').config();
 import indexRouter from './routes/index';
 import domainsRouter from './routes/domains';
 import { start } from './services/unscannedDomainsScheduler';
-import { securityMiddleware, xssFilter } from './middlewares/security';
+import { securityMiddleware, xssFilter, rateLimiter } from './middlewares/security';
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(xssFilter, securityMiddleware)
+app.use(xssFilter, securityMiddleware, rateLimiter)
 app.use('/', indexRouter);
 app.use('/domains', domainsRouter);
 
--- src/middlewares/security.ts
+++ src/middlewares/security.ts
@@ -1,5 +1,6 @@
 import helmet from 'helmet';
 require('dotenv').config();
+import rateLimit from 'express-rate-limit';
 
 export const securityMiddleware = helmet.contentSecurityPolicy({
   directives: {
@@ -9,5 +10,14 @@ export const securityMiddleware = helmet.contentSecurityPolicy({
     ],
   }
 });
+
+
+export const rateLimiter = rateLimit({
+	windowMs: 60 * 1000,
+	max: 15,
+	standardHeaders: true,
+	legacyHeaders: false,
+	message: JSON.stringify({ statusCode: 500, message: 'exceeded rate limit.' }),
+})
 
 export const xssFilter = helmet.xssFilter();
